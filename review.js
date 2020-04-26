493a8c9247bc77339a1d31e360804d61e0019e0b
JaeseongPark <qkrwotjd1445@naver.com>
2020-04-26 18:52:55 +0900

    xlsx 파일로부터 여러 url로부터 크롤링할 수 있게 수정

--- review.js
+++ review.js
@@ -1,13 +1,21 @@
 const puppeteer = require("puppeteer"); // 설치된 puppeteer 모듈
 const { Parser } = require('json2csv');
 const fs = require('fs');
+const XLSX = require('xlsx');
 
-(async () => {
+// console.log('b : ', firstWSheet);
+
+
+const reviewCrawling = async (pageUrl, fileName) => {
     try {
+        const workbook = XLSX.readFile('Zarina_url.xlsx');
+        const sheet_name_list = workbook.SheetNames;
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
         const browser = await puppeteer.launch(); // headless 브라우저 실행
         const page = await browser.newPage(); // 새로운 페이지 열기
         // 해당 URL에 접속
-        await page.goto("https://www.wildberries.ru/catalog/9432265/otzyvy?page={}&field=Date&order=Desc&link=4520791");
+        // await page.goto("https://www.wildberries.ru/catalog/5966627/otzyvy?page={}&field=Date&order=Desc&link=4773432");
+        await page.goto(pageUrl);
         await page.click('a.show-more');
         // evaluate() 함수를 이용해 History table을 선택하고 반복문으로 내용을 빈배열에 추가한다
         const reactHistory = await page.evaluate(async() => {
@@ -31,9 +39,8 @@ const fs = require('fs');
                     working = false;
                 }
             }
-            // for (let i = 0; i < 6; i ++) {
-            //     await foo();
-            // }
+            
+            
             // 반복문으로 <tbody> 내용 객체 형식으로 빈 배열에 추가
             let scrappedData = []; // 스크래핑 내용 담을 빈 배열
             for (let i = 0; i < commentsDOM.length; i++) {
@@ -56,7 +63,7 @@ const fs = require('fs');
         const parser = new Parser();
         const csv = parser.parse(reactHistory);
         console.log('csv 파일 : ', csv);
-        fs.writeFile('./data.csv', csv, 'utf8', (err) => {
+        fs.writeFile(`./${fileName}.csv`, csv, 'utf8', (err) => {
             if (err) {
                 console.log('에러 : ', err);
             }
@@ -65,4 +72,12 @@ const fs = require('fs');
     } catch (err) {
         console.log(err);
     }
-})();
+};
+
+const workbook = XLSX.readFile('Zarina_url.xlsx');
+const sheet_name_list = workbook.SheetNames;
+const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+
+for (let i = 0; i < data.length; i++) {
+    reviewCrawling(data[i].Url, data[i].Item_Id);
+}
